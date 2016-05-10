1ed7cb20e6b0c336ae874bd40740a1db3371f1ec
lightingghost <zhenp3ngzhou@gmail.com>
2016-05-09 18:21:44 -0700

    1.4 para tune

--- auto_sum_lstm.py
+++ auto_sum_lstm.py
@@ -20,7 +20,7 @@ _num_embed      = 300
 _num_label      = _dict_len + 2
 _dropout        = 0.
 #opt para
-_learning_rate  = 0.0005
+_learning_rate  = 0.004
 #training para
 _devs           = [mx.gpu()]
 _batch_size     = 32
@@ -102,11 +102,11 @@ def Perplexity(label, pred):
 # def Perplexity(label, pred):
 #     return np.exp(np.mean(pred))
     
-opt = mx.optimizer.AdaDelta(learning_rate=_learning_rate)
+opt = mx.optimizer.Adam(learning_rate=_learning_rate)
 
 pre_trained = {'embed_weight': embed_weight}
 init = mx.initializer.Load(pre_trained,
-                           default_init=mx.initializer.Normal())
+                           default_init=mx.initializer.Xaiver(magnitude=2))
 group2ctx = {'embed'      : mx.cpu(0),
              'preproc'    : mx.cpu(1),
              'sent_layers': mx.gpu(),
